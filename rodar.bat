d05f1179a7812478326c812fbdd820e78cc5fc19
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-03-24 16:08:21 -0300

    log que rodou!

--- rodar.bat
+++ rodar.bat
@@ -1,2 +1,9 @@
 @echo off
-"g:\Meu Drive\offtrade\.venv\Scripts\python.exe" "g:\Meu Drive\offtrade\main.py"
+echo [%date% %time%] Iniciando... >> "G:\Meu Drive\offtrade\offtrade.log"
+"g:\Meu Drive\offtrade\.venv\Scripts\python.exe" "g:\Meu Drive\offtrade\main.py" >> "G:\Meu Drive\offtrade\offtrade.log" 2>&1
+if %errorlevel% == 0 (
+    echo [%date% %time%] OK >> "G:\Meu Drive\offtrade\offtrade.log"
+) else (
+    echo [%date% %time%] ERRO (codigo %errorlevel%) >> "G:\Meu Drive\offtrade\offtrade.log"
+)
+echo ---------------------------------------- >> "G:\Meu Drive\offtrade\offtrade.log"
