8f5f2cc3b8893a22837fdbd3cab75635cd0884c3
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-03-27 10:00:48 -0300

    Atualiza metas_data.js - 27/03/2026

--- metas_data.js
+++ metas_data.js
@@ -2,7 +2,7 @@
 
 const METAS_DATA = {
   "mes": "March 2026",
-  "atualizado_em": "27/03/2026 09:22",
+  "atualizado_em": "27/03/2026 10:00",
   "vendedores": [
     {
       "nome": "MARIA LUIZA",
