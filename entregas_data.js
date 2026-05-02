730099dc40b9999eb49f90c42ecd3e0ad8772451
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-05-02 12:38:51 -0300

    Atualiza entregas_data.js - 02/05/2026

--- entregas_data.js
+++ entregas_data.js
@@ -1,5 +1,271 @@
 const ENTREGAS_DATA = {
-  "atualizado_em": "01/05/2026 15:31",
-  "data_rota": "01.05",
-  "vendedores": []
+  "atualizado_em": "02/05/2026 12:38",
+  "data_rota": "02.05",
+  "vendedores": [
+    {
+      "nome": "ADEILSON GONÇALVES",
+      "em_rota": [],
+      "nao_emitido": [],
+      "emitido_s_rota": [
+        {
+          "numped": "431000863",
+          "numnota": "390074.0",
+          "data": "02/05/2026",
+          "cliente": "NOBRE BOI PADARIA E MERCEARIA LTDA",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 473.4,
+          "itens": [
+            {
+              "desc": "MOVING HYDRO P MELANCIA LIMAO 12X500ML",
+              "qt": 1,
+              "val": 94.68
+            },
+            {
+              "desc": "MOVING HYDRO PROTEIN LIMAO 12X500ML",
+              "qt": 1,
+              "val": 94.68
+            },
+            {
+              "desc": "MOVING HYDRO PROTEIN TANGERINA 12X500ML",
+              "qt": 1,
+              "val": 94.68
+            },
+            {
+              "desc": "MOVING HYDRO PROTEIN UVA 12X500ML",
+              "qt": 1,
+              "val": 94.68
+            },
+            {
+              "desc": "MOVING HYDRO PROTEIN F VERMELHA 12X500ML",
+              "qt": 1,
+              "val": 94.68
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "nome": "JORGE MACIEL",
+      "em_rota": [],
+      "nao_emitido": [
+        {
+          "numped": "379000872",
+          "numnota": "",
+          "data": "02/05/2026",
+          "cliente": "M T FERREIRA DA SILVA",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 292.8,
+          "itens": [
+            {
+              "desc": "PIRASSUNUNGA 51 965 ML",
+              "qt": 12,
+              "val": 142.8
+            },
+            {
+              "desc": "CACHACA VELHO BARREIRO 910 ML",
+              "qt": 12,
+              "val": 150.0
+            }
+          ]
+        },
+        {
+          "numped": "379000871",
+          "numnota": "",
+          "data": "02/05/2026",
+          "cliente": "55.529.476 AUREA DOS SANTOS TEIXEIRA",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 148.2,
+          "itens": [
+            {
+              "desc": "GIN INVICTUS TRADICIONAL 900ML",
+              "qt": 6,
+              "val": 89.4
+            },
+            {
+              "desc": "SABOR DO SABOR WHISKY + COMBO TRAD",
+              "qt": 6,
+              "val": 58.8
+            }
+          ]
+        }
+      ],
+      "emitido_s_rota": []
+    },
+    {
+      "nome": "LEANDRO SOUZA",
+      "em_rota": [],
+      "nao_emitido": [
+        {
+          "numped": "450000106",
+          "numnota": "",
+          "data": "02/05/2026",
+          "cliente": "BOM DE PRECO COMERCIO E DISTRIBUIDORA DE ALIMENTOS LTDA",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 115.8,
+          "itens": [
+            {
+              "desc": "RUM MONTILLA CARTA BRANCA 1000ML",
+              "qt": 6,
+              "val": 115.8
+            }
+          ]
+        },
+        {
+          "numped": "450000107",
+          "numnota": "",
+          "data": "02/05/2026",
+          "cliente": "SUPERMERCADO PADRAO DO FONSECA LTDA.",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 946.8,
+          "itens": [
+            {
+              "desc": "CHANDON RESERVE BRUT ROSE 750ML",
+              "qt": 12,
+              "val": 946.8
+            }
+          ]
+        },
+        {
+          "numped": "450000108",
+          "numnota": "",
+          "data": "02/05/2026",
+          "cliente": "SUPERMERCADO PADRAO DO FONSECA EIRELI",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 946.8,
+          "itens": [
+            {
+              "desc": "CHANDON RESERVE BRUT ROSE 750ML",
+              "qt": 12,
+              "val": 946.8
+            }
+          ]
+        },
+        {
+          "numped": "450000104",
+          "numnota": "",
+          "data": "02/05/2026",
+          "cliente": "BOM DE PRECO COMERCIO E DISTRIBUIDORA DE ALIMENTOS LTDA",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 115.8,
+          "itens": [
+            {
+              "desc": "RUM MONTILLA CARTA BRANCA 1000ML",
+              "qt": 6,
+              "val": 115.8
+            }
+          ]
+        }
+      ],
+      "emitido_s_rota": []
+    },
+    {
+      "nome": "MARILENA TRAGEL",
+      "em_rota": [],
+      "nao_emitido": [
+        {
+          "numped": "156003242",
+          "numnota": "",
+          "data": "02/05/2026",
+          "cliente": "AUTO POSTO DE COMBUSTIVEIS HUMAITA LTDA",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 418.0,
+          "itens": [
+            {
+              "desc": "AGUA MINERAL MINALBA S/ GAS 12X510ML",
+              "qt": 20,
+              "val": 418.0
+            }
+          ]
+        }
+      ],
+      "emitido_s_rota": []
+    },
+    {
+      "nome": "RAPHAEL SANTOS - OFF TRADE",
+      "em_rota": [],
+      "nao_emitido": [
+        {
+          "numped": "155003396",
+          "numnota": "",
+          "data": "02/05/2026",
+          "cliente": "QUALITYFOOD LOGISTICA GASTRONOMICA LTDA",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 765.6,
+          "itens": [
+            {
+              "desc": "RUM BACARDI SUPERIOR 980ML",
+              "qt": 24,
+              "val": 765.6
+            }
+          ]
+        },
+        {
+          "numped": "155003397",
+          "numnota": "",
+          "data": "02/05/2026",
+          "cliente": "AGROCOMPEC MARTHI LTDA",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 457.7,
+          "itens": [
+            {
+              "desc": "WHISKY OLD PARR 12Y 1 L",
+              "qt": 1,
+              "val": 154.9
+            },
+            {
+              "desc": "JACK DANIELS HONEY 1 L",
+              "qt": 1,
+              "val": 122.9
+            },
+            {
+              "desc": "WHISKY CHIVAS XV GOLD 700ML",
+              "qt": 1,
+              "val": 179.9
+            }
+          ]
+        }
+      ],
+      "emitido_s_rota": []
+    }
+  ]
 };
