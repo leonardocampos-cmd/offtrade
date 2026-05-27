8f284461d6fe4422c48b1a670d9de7dd0b8719f9
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-05-27 09:45:02 -0300

    Atualiza amarula_data.js - 27/05/2026

---
+++ amarula_data.js
@@ -0,0 +1,63 @@
+const AMARULA_DATA = {
+  "atualizado_em": "27/05/2026 09:45",
+  "periodo": {
+    "ini": "25/05/2026",
+    "fim": "25/06/2026"
+  },
+  "premio": 3000,
+  "total_vendedores": 6,
+  "total_positivacao": 6,
+  "total_faturamento": 4330.2,
+  "ranking_positivacao": [
+    {
+      "vendedor": "CARLOS ARTHUR NETO",
+      "valor": 1
+    },
+    {
+      "vendedor": "INGRID ARAUJO",
+      "valor": 1
+    },
+    {
+      "vendedor": "JORGE MACIEL - OFF TRADE",
+      "valor": 1
+    },
+    {
+      "vendedor": "MARILENA TRAGEL - OFF TRADE",
+      "valor": 1
+    },
+    {
+      "vendedor": "NÁTALI DE OLIVEIRA - OFF TRADE",
+      "valor": 1
+    },
+    {
+      "vendedor": "THIAGO NASCIMENTO",
+      "valor": 1
+    }
+  ],
+  "ranking_faturamento": [
+    {
+      "vendedor": "JORGE MACIEL - OFF TRADE",
+      "valor": 3506.1
+    },
+    {
+      "vendedor": "NÁTALI DE OLIVEIRA - OFF TRADE",
+      "valor": 267.7
+    },
+    {
+      "vendedor": "MARILENA TRAGEL - OFF TRADE",
+      "valor": 197.8
+    },
+    {
+      "vendedor": "INGRID ARAUJO",
+      "valor": 177.8
+    },
+    {
+      "vendedor": "THIAGO NASCIMENTO",
+      "valor": 91.9
+    },
+    {
+      "vendedor": "CARLOS ARTHUR NETO",
+      "valor": 88.9
+    }
+  ]
+};
