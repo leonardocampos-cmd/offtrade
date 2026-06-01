89533a4ce155fa26c5b5d5c44ff081103e9fee0e
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-06-01 10:01:21 -0300

    Atualiza entregas_data.js - 01/06/2026

--- entregas_data.js
+++ entregas_data.js
@@ -1,11 +1,98 @@
 const ENTREGAS_DATA = {
-  "atualizado_em": "01/06/2026 09:03",
+  "atualizado_em": "01/06/2026 10:01",
   "data_rota": "01.06",
   "vendedores": [
+    {
+      "nome": "FABIO VALOTTI",
+      "em_rota": [],
+      "nao_emitido": [
+        {
+          "numped": "378000396",
+          "numnota": "",
+          "data": "01/06/2026",
+          "cliente": "TO NA SERRA BAR E RESTAURANTE LTDA",
+          "placa": "",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "Favor entregar dia 02/06",
+          "total": 378.8,
+          "itens": [
+            {
+              "desc": "MOVING HYDRO P MELANCIA LIMAO 12X500ML",
+              "qt": 1,
+              "val": 94.7
+            },
+            {
+              "desc": "MOVING HYDRO PROTEIN TANGERINA 12X500ML",
+              "qt": 1,
+              "val": 94.7
+            },
+            {
+              "desc": "MOVING HYDRO PROTEIN LIMAO 12X500ML",
+              "qt": 1,
+              "val": 94.7
+            },
+            {
+              "desc": "MOVING HYDRO PROTEIN F VERMELHA 12X500ML",
+              "qt": 1,
+              "val": 94.7
+            }
+          ]
+        }
+      ],
+      "emitido_s_rota": []
+    },
     {
       "nome": "JORGE MACIEL",
       "em_rota": [],
       "nao_emitido": [
+        {
+          "numped": "379001030",
+          "numnota": "",
+          "data": "01/06/2026",
+          "cliente": "MONTES DOCES MARECHAL HERMES LTDA",
+          "placa": "",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 72.12,
+          "itens": [
+            {
+              "desc": "VEJA PERF CON LAVANDA 12X100ML",
+              "qt": 1,
+              "val": 72.12
+            }
+          ]
+        },
+        {
+          "numped": "379001029",
+          "numnota": "",
+          "data": "01/06/2026",
+          "cliente": "MONTES DOCES MARECHAL HERMES LTDA",
+          "placa": "",
+          "rota": "",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 185.8,
+          "itens": [
+            {
+              "desc": "ST PIERRE ZERO LATA RED MINT LT 24X310ML",
+              "qt": 1,
+              "val": 95.9
+            },
+            {
+              "desc": "AMARULA C LIQUEUR 750 ML + 1 COPO",
+              "qt": 1,
+              "val": 89.9
+            }
+          ]
+        },
         {
           "numped": "379001028",
           "numnota": "",
