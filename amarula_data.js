c6b103d57c62a1c3282fbf0ee1db07317dce66ca
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-06-01 10:49:12 -0300

    Atualiza amarula_data.js - 01/06/2026

--- amarula_data.js
+++ amarula_data.js
@@ -1,21 +1,21 @@
 const AMARULA_DATA = {
-  "atualizado_em": "01/06/2026 10:12",
+  "atualizado_em": "01/06/2026 10:49",
   "periodo": {
     "ini": "25/05/2026",
     "fim": "25/06/2026"
   },
   "premio": 3000,
-  "total_vendedores": 13,
-  "total_positivacao": 18,
-  "total_faturamento": 26113.9,
+  "total_vendedores": 20,
+  "total_positivacao": 27,
+  "total_faturamento": 47735.9,
   "ranking_positivacao": [
     {
-      "vendedor": "MICHELE ALCANTARA",
+      "vendedor": "RC",
       "valor": 3
     },
     {
-      "vendedor": "JORGE MACIEL",
-      "valor": 2
+      "vendedor": "MICHELE ALCANTARA",
+      "valor": 3
     },
     {
       "vendedor": "CARLOS ARTHUR NETO",
@@ -26,19 +26,19 @@ const AMARULA_DATA = {
       "valor": 2
     },
     {
-      "vendedor": "BEES",
-      "valor": 1
-    },
-    {
-      "vendedor": "INGRID ARAUJO",
-      "valor": 1
+      "vendedor": "JORGE MACIEL",
+      "valor": 2
     },
     {
       "vendedor": "DIOGO RAPOSO",
       "valor": 1
     },
     {
-      "vendedor": "LAIS POPPE VILLANOVA",
+      "vendedor": "BEES",
+      "valor": 1
+    },
+    {
+      "vendedor": "BIANCA RODRIGUES",
       "valor": 1
     },
     {
@@ -46,11 +46,39 @@ const AMARULA_DATA = {
       "valor": 1
     },
     {
+      "vendedor": "LAIS POPPE VILLANOVA",
+      "valor": 1
+    },
+    {
+      "vendedor": "INGRID ARAUJO",
+      "valor": 1
+    },
+    {
+      "vendedor": "FERNANDO CESAR",
+      "valor": 1
+    },
+    {
       "vendedor": "MARILENA TRAGEL",
       "valor": 1
     },
     {
+      "vendedor": "LUCIANO TOSCHI - OFF TRADE",
+      "valor": 1
+    },
+    {
       "vendedor": "NATALI OLIVEIRA",
+      "valor": 1
+    },
+    {
+      "vendedor": "PEDRO HENRIQUE DUARTE",
+      "valor": 1
+    },
+    {
+      "vendedor": "RAPHAELA RIBAS",
+      "valor": 1
+    },
+    {
+      "vendedor": "RAFAEL DE OLIVEIRA",
       "valor": 1
     },
     {
@@ -66,6 +94,14 @@ const AMARULA_DATA = {
     {
       "vendedor": "VENDEDOR 09",
       "valor": 18501.4
+    },
+    {
+      "vendedor": "RC",
+      "valor": 10546.8
+    },
+    {
+      "vendedor": "FERNANDO CESAR",
+      "valor": 10308.0
     },
     {
       "vendedor": "JORGE MACIEL",
@@ -84,6 +120,10 @@ const AMARULA_DATA = {
       "valor": 587.4
     },
     {
+      "vendedor": "BIANCA RODRIGUES",
+      "valor": 287.7
+    },
+    {
       "vendedor": "THIAGO NASCIMENTO",
       "valor": 269.7
     },
@@ -96,11 +136,15 @@ const AMARULA_DATA = {
       "valor": 197.8
     },
     {
-      "vendedor": "DIOGO RAPOSO",
-      "valor": 177.8
+      "vendedor": "LUCIANO TOSCHI - OFF TRADE",
+      "valor": 191.8
     },
     {
       "vendedor": "CARLOS ARTHUR NETO",
+      "valor": 177.8
+    },
+    {
+      "vendedor": "DIOGO RAPOSO",
       "valor": 177.8
     },
     {
@@ -110,6 +154,18 @@ const AMARULA_DATA = {
     {
       "vendedor": "JOÃO VITOR BRAGA",
       "valor": 171.8
+    },
+    {
+      "vendedor": "PEDRO HENRIQUE DUARTE",
+      "valor": 95.9
+    },
+    {
+      "vendedor": "RAPHAELA RIBAS",
+      "valor": 95.9
+    },
+    {
+      "vendedor": "RAFAEL DE OLIVEIRA",
+      "valor": 95.9
     },
     {
       "vendedor": "BEES",
