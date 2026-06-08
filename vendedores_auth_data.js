432569454cec021dd596adcca74ee97d294352d0
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-06-08 15:22:00 -0300

    Auto-update 08/06/2026 15:21

--- vendedores_auth_data.js
+++ vendedores_auth_data.js
@@ -1,23 +1,39 @@
-// Gerado por exportacao_vendedores_auth.py
-// Execute o script Python para preencher os e-mails automaticamente via SPON.PCUSUARI
+// Gerado em 08/06/2026 15:21
 const VENDEDORES_AUTH = {
-  "275": { "nome": "MARIA LUIZA",         "email": "" },
-  "156": { "nome": "MARILENA TRAGEL",     "email": "" },
-  "412": { "nome": "BARBARA CABRAL",      "email": "" },
-  "153": { "nome": "ANGELO SUZART",       "email": "" },
-  "158": { "nome": "JOSE MARCELO LORES",  "email": "" },
-  "238": { "nome": "DANIELLE MOURA",      "email": "" },
-  "419": { "nome": "NATALI OLIVEIRA",     "email": "" },
-  "144": { "nome": "DIOGO RAPOSO",        "email": "" },
-  "450": { "nome": "LEANDRO SOUZA",       "email": "" },
-  "417": { "nome": "DIRLEI",              "email": "" },
-  "439": { "nome": "MATEUS",              "email": "" },
-  "420": { "nome": "KELLY RAMOS (KAROL)", "email": "" },
-  "431": { "nome": "ADEILSON GONÇALVES",  "email": "" },
-  "379": { "nome": "JORGE MACIEL",        "email": "" },
-  "471": { "nome": "ANA CLARA PASSOS",    "email": "" },
-  "378": { "nome": "FABIO VALOTTI",       "email": "" },
-  "91":  { "nome": "VIVIANE ALVES",       "email": "" },
-  "446": { "nome": "FRANCISCO MACIEL",    "email": "" },
-  "174": { "nome": "ALLAN PAES",          "email": "" }
+  "391": {
+    "nome": "ANDRE SILVA - OFF TRADE",
+    "email": "andre.silva@rigarr.com.br"
+  },
+  "399": {
+    "nome": "BRYAN PALOPOLI - OFF TRADE",
+    "email": "bryan.palopoli@castas.com.br"
+  },
+  "371": {
+    "nome": "CARLOS DE ALMEIDA - OFF TRADE",
+    "email": "carlos.almeida@rigarr.com.br"
+  },
+  "432": {
+    "nome": "DIEGO PAULINO - OFF TRADE",
+    "email": "diego.paulo@rigarr.com.br"
+  },
+  "405": {
+    "nome": "JESSICA FREITAS - OFF TRADE",
+    "email": "jessica.freitas@rigarr.com.br"
+  },
+  "438": {
+    "nome": "MARCIA RITA - OFF TRADE",
+    "email": "marcia.veloso@rigarr.com.br"
+  },
+  "318": {
+    "nome": "MARCUS TANAMACHI - OFF TRADE",
+    "email": "marcus.tanamachi@rigarr.com.br"
+  },
+  "367": {
+    "nome": "FABIANO RIBEIRO - OFF TRADE",
+    "email": "fabiano.ribeiro@rigarr.com.br"
+  },
+  "356": {
+    "nome": "ANA PAULA - OFF TRADE",
+    "email": "ana.gilha@rigarr.com.br"
+  }
 };
