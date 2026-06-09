d31a1bf7c0bac7cb8156eef35390ce01d6b35c8a
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-06-09 10:02:24 -0300

    Atualiza metas_data.js + vendas_data.js - 09/06/2026

--- metas_data.js
+++ metas_data.js
@@ -1,7 +1,7 @@
 // Gerado automaticamente pelo notebook analisedados.ipynb
 
 const METAS_DATA = {
-  "atualizado_em": "09/06/2026 10:00",
+  "atualizado_em": "09/06/2026 10:02",
   "meses": [
     "Jun/26",
     "Mai/26",
