85c4cd46c755ffc074e24e092297afa36c919162
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-06-11 10:24:13 -0300

    Auto-update 11/06/2026 10:24

--- vendedores_auth_data.js
+++ vendedores_auth_data.js
@@ -1,67 +1,67 @@
-// Gerado em 11/06/2026 10:03
+// Gerado em 11/06/2026 10:23
 const VENDEDORES_AUTH = {
   "156": {
-    "nome": "MARILENA TRAGEL",
+    "nome": "MARILENA TRAGEL - OFF TRADE",
     "email": "marilena.tragel@rigarr.com.br"
   },
   "144": {
-    "nome": "DIOGO RAPOSO",
+    "nome": "DIOGO RAPOSO - OFF TRADE",
     "email": "diogo.raposo@rigarr.com.br"
   },
   "174": {
-    "nome": "ALLAN PAES",
+    "nome": "ALLAN PAES - OFF TRADE",
     "email": "allan.correa@rigarr.com.br"
   },
   "153": {
-    "nome": "ANGELO SUZART",
+    "nome": "ANGELO NEVES SUZART - OFF TRADE",
     "email": "angelo.suzart@rigarr.com.br"
   },
   "158": {
-    "nome": "JOSE MARCELO LORES",
+    "nome": "JOSE MARCELO CARDOSO - OFF TRADE",
     "email": "jose.cardoso@rigarr.com.br"
   },
   "431": {
-    "nome": "ADEILSON GONÇALVES",
+    "nome": "ADEILSON GONÇALVEZ - OFF TRADE",
     "email": "adeilson.rigarr@gmail.com"
   },
   "439": {
-    "nome": "MATEUS",
+    "nome": "MATEUS CARDOSO - OFF TRADE",
     "email": "mateus.cardoso@rigarr.com.br"
   },
   "471": {
-    "nome": "ANA CLARA PASSOS",
+    "nome": "ANA CLARA FASSANO - OFF TRADE",
     "email": "ana.fassano@rigarr.com.br"
   },
   "450": {
-    "nome": "LEANDRO SOUZA",
+    "nome": "LEANDRO SOUZA - OFF TRADE",
     "email": "leandro.souza@rigarr.com.br"
   },
   "412": {
-    "nome": "BARBARA CABRAL",
+    "nome": "BARBARA CABRAL - OFF TRADE",
     "email": "barbara.cabral@rigarr.com.br"
   },
   "238": {
-    "nome": "DANIELLE MOURA",
+    "nome": "DANIELLE MOURA - OFF TRADE",
     "email": "danielle.soares@rigarr.com.br"
   },
   "275": {
-    "nome": "MARIA LUIZA",
+    "nome": "MARIA LUIZA - OFF TRADE",
     "email": "marialuiza.melo@rigarr.com.br"
   },
   "378": {
-    "nome": "FABIO VALOTTI",
+    "nome": "FABIO VALOTTI - OFF TRADE",
     "email": "fabio.valotti@rigarr.com.br"
   },
   "379": {
-    "nome": "JORGE MACIEL",
+    "nome": "JORGE MACIEL - OFF TRADE",
     "email": "jorge.cunha@rigarr.com.br"
   },
   "419": {
-    "nome": "NATALI OLIVEIRA",
+    "nome": "NÁTALI DE OLIVEIRA - OFF TRADE",
     "email": "natali.fontes@rigarr.com.br"
   },
   "417": {
-    "nome": "DIRLEI",
+    "nome": "DIRLEI ARAUJO - OFF TRADE",
     "email": "dirlei.vieira@rigarr.com.br"
   }
 };
