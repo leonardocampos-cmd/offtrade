a2d3f4fdfdf2f50effb4d452e9edc5ba16cde542
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-06-25 14:41:37 -0300

    Atualiza coluna de preço na tabela de preços

--- conferencia_preco.py
+++ conferencia_preco.py
@@ -31,7 +31,7 @@ tabela_preco_on = pd.read_excel(
 col_on_excluir = ['Unnamed: 0', 'COD BRASIL', 'PRODUTOS', 'CATEGORIA', 'FORNECEDOR', 'CX C/', 'PLT C/', 
                   'CONDIÇÃO PROMO', 'BASE RECOMP.', 'EM FALTA?']
 tabela_preco_on.drop(columns=[c for c in col_on_excluir if c in tabela_preco_on.columns], inplace=True)
-tabela_preco_on['PREÇO TABELA'] = pd.to_numeric(tabela_preco_on['PREÇO TABELA'].str.replace(',', '.'), errors='coerce').round(2)
+tabela_preco_on['PREÇO'] = pd.to_numeric(tabela_preco_on['PREÇO'].str.replace(',', '.'), errors='coerce').round(2)
 
 # --- TABELA PROMO ---
 tabela_promo = pd.read_excel(
