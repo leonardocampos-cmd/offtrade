c5c378e32531d61400eba9e6c9d22bf1fe04a1ac
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-06-26 09:31:44 -0300

    Atualiza nao_pos_mg_data.js - 26/06/2026

--- nao_pos_mg_data.js
+++ nao_pos_mg_data.js
@@ -1,7 +1,7 @@
 // Gerado automaticamente pelo exportacao_nao_pos_mg.py
 
 const NAO_POS_MG_DATA = {
-  "atualizado_em": "26/06/2026 09:18",
+  "atualizado_em": "26/06/2026 09:31",
   "por_vendedor": {
     "BRUNA CASTRO MELLO": [
       {
