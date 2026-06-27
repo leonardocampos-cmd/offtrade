58cf25c5fa2d15656370e8920bb293ea0deeddc7
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-06-27 15:22:09 -0300

    Atualiza vendas_sp_data.js - 27/06/2026

--- vendas_sp_data.js
+++ vendas_sp_data.js
@@ -1,7 +1,7 @@
 // Gerado automaticamente pelo exportacao_sp.py
 
 const VENDAS_SP_DATA = {
-  "atualizado_em": "27/06/2026 15:12",
+  "atualizado_em": "27/06/2026 15:22",
   "meses": [
     "Jun/26",
     "Mai/26",
