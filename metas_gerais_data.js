395f97fca1dbf11ada12d9a1b6a42e7b3ae79d4e
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-07-01 14:37:41 -0300

    Atualiza metas_gerais_data.js - 01/07/2026

--- metas_gerais_data.js
+++ metas_gerais_data.js
@@ -1,5 +1,5 @@
 const METAS_GERAIS_DATA = {
-  "atualizado_em": "01/07/2026 10:57",
+  "atualizado_em": "01/07/2026 14:37",
   "mes": "Jul/26",
   "mes_ant": "Jun/26",
   "dias_corridos": 1,
@@ -8,7 +8,7 @@ const METAS_GERAIS_DATA = {
   "resumo": {
     "fat": NaN,
     "fat_ant": 216453.6,
-    "pos": 0,
+    "pos": 1,
     "pos_ant": 87
   },
   "total": {
@@ -56,13 +56,25 @@ const METAS_GERAIS_DATA = {
       "estado": "MG",
       "label": "Minas Gerais",
       "meta": 2100000.0,
-      "fat": NaN,
+      "fat": 478.8,
       "fat_ant": 37721.9,
-      "pos": 0,
-      "pct": NaN,
-      "nec_dia": NaN,
+      "pos": 1,
+      "pct": 0.0,
+      "nec_dia": 69984.04,
       "pos_ant": 21
     }
   ],
-  "industrias": []
+  "industrias": [
+    {
+      "fantasia": "SEM FORNECEDOR",
+      "fat": 478.8,
+      "fat_ant": 21739.32,
+      "por_estado": {
+        "RJ": 0.0,
+        "SP": 0.0,
+        "ES": 0.0,
+        "MG": 478.8
+      }
+    }
+  ]
 };
