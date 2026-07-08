1f0f294a2a90a73d28dde0e01c76161675f47492
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-07-08 20:58:25 -0300

    Atualiza crusoe_data.js - 08/07/2026

---
+++ crusoe_data.js
@@ -0,0 +1,45 @@
+const CRUSOE_DATA = {
+  "atualizado_em": "08/07/2026 20:58",
+  "periodo": {
+    "ini": "01/07/2026",
+    "fim": "31/08/2026"
+  },
+  "premio": 800,
+  "vendedores": [
+    {
+      "rca": 275,
+      "vendedor": "Maria Luiza",
+      "pedidos": 5,
+      "novos_skus": 22,
+      "reativacoes": 1,
+      "faturamento": 86280.41,
+      "meta_definida": false,
+      "meta_valor": null,
+      "meta_atingida": false,
+      "pontos_pedidos": 5,
+      "pontos_novos_skus": 110,
+      "pontos_reativacoes": 5,
+      "pontos_meta": 0,
+      "pontos_total": 120
+    },
+    {
+      "rca": 158,
+      "vendedor": "Jose Marcelo Cardoso",
+      "pedidos": 1,
+      "novos_skus": 3,
+      "reativacoes": 0,
+      "faturamento": 1455.18,
+      "meta_definida": false,
+      "meta_valor": null,
+      "meta_atingida": false,
+      "pontos_pedidos": 1,
+      "pontos_novos_skus": 15,
+      "pontos_reativacoes": 0,
+      "pontos_meta": 0,
+      "pontos_total": 16
+    }
+  ],
+  "fontes_indisponiveis": [
+    "CASTAS"
+  ]
+};
