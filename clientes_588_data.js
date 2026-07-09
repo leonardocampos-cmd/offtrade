75a62a6ce5092b1cd6eba5833138506eab1bfea0
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-07-09 09:20:02 -0300

    Atualiza clientes_588_data.js - 09/07/2026

--- clientes_588_data.js
+++ clientes_588_data.js
@@ -1,5 +1,5 @@
 const CLIENTES_588_DATA = {
-  "atualizado_em": "09/07/2026 08:52",
+  "atualizado_em": "09/07/2026 09:20",
   "rca_ref": 588,
   "periodo_fid": "Jan/26 – Abr/26",
   "mes_migracao": "Mai/26",
