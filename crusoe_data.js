a615b7e6d3b3d464ffe15637dc2ad8091e31b160
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-07-08 21:21:56 -0300

    Atualiza crusoe_data.js - 08/07/2026

--- crusoe_data.js
+++ crusoe_data.js
@@ -1,45 +1,225 @@
 const CRUSOE_DATA = {
-  "atualizado_em": "08/07/2026 20:58",
+  "atualizado_em": "08/07/2026 21:21",
   "periodo": {
     "ini": "01/07/2026",
     "fim": "31/08/2026"
   },
-  "premio": 800,
-  "vendedores": [
+  "times": [
     {
-      "rca": 275,
-      "vendedor": "Maria Luiza",
-      "pedidos": 5,
-      "novos_skus": 22,
-      "reativacoes": 1,
-      "faturamento": 86280.41,
-      "meta_definida": false,
-      "meta_valor": null,
-      "meta_atingida": false,
-      "pontos_pedidos": 5,
-      "pontos_novos_skus": 110,
-      "pontos_reativacoes": 5,
-      "pontos_meta": 0,
-      "pontos_total": 120
+      "id": "KEY_ACCOUNT",
+      "nome": "Key Account",
+      "premio": 800,
+      "criterios": [
+        {
+          "label": "por pedido",
+          "valor": "+1 pt"
+        },
+        {
+          "label": "novo SKU (60d)",
+          "valor": "+5 pts"
+        },
+        {
+          "label": "reativação (60d)",
+          "valor": "+5 pts"
+        },
+        {
+          "label": "meta batida no mês",
+          "valor": "+4 pts"
+        }
+      ],
+      "vendedores": [
+        {
+          "rca": 275,
+          "vendedor": "Maria Luiza",
+          "pedidos": 5,
+          "novos_skus": 22,
+          "reativacoes": 1,
+          "faturamento": 86280.41,
+          "meta_definida": false,
+          "meta_valor": null,
+          "meta_atingida": false,
+          "pontos_pedidos": 5,
+          "pontos_novos_skus": 110,
+          "pontos_reativacoes": 5,
+          "pontos_meta": 0,
+          "pontos_total": 120
+        },
+        {
+          "rca": 158,
+          "vendedor": "Jose Marcelo Cardoso",
+          "pedidos": 1,
+          "novos_skus": 3,
+          "reativacoes": 0,
+          "faturamento": 1455.18,
+          "meta_definida": false,
+          "meta_valor": null,
+          "meta_atingida": false,
+          "pontos_pedidos": 1,
+          "pontos_novos_skus": 15,
+          "pontos_reativacoes": 0,
+          "pontos_meta": 0,
+          "pontos_total": 16
+        }
+      ]
     },
     {
-      "rca": 158,
-      "vendedor": "Jose Marcelo Cardoso",
-      "pedidos": 1,
-      "novos_skus": 3,
-      "reativacoes": 0,
-      "faturamento": 1455.18,
-      "meta_definida": false,
-      "meta_valor": null,
-      "meta_atingida": false,
-      "pontos_pedidos": 1,
-      "pontos_novos_skus": 15,
-      "pontos_reativacoes": 0,
-      "pontos_meta": 0,
-      "pontos_total": 16
+      "id": "ATACAREJO",
+      "nome": "Atacarejo",
+      "premio": 800,
+      "criterios": [
+        {
+          "label": "positivação (novo/reativação 60d)",
+          "valor": "+5 pts"
+        },
+        {
+          "label": "novo SKU",
+          "valor": "+4 pts"
+        },
+        {
+          "label": "pedido R$500-1000",
+          "valor": "+1 pt"
+        },
+        {
+          "label": "pedido R$1001-3000",
+          "valor": "+2 pts"
+        },
+        {
+          "label": "pedido acima de R$3001",
+          "valor": "+3 pts"
+        }
+      ],
+      "vendedores": [
+        {
+          "rca": 412,
+          "vendedor": "Barbara Cabral",
+          "pedidos": 17,
+          "positivacoes": 0,
+          "novos_skus": 123,
+          "faturamento": 42362.09,
+          "pontos_positivacao": 0,
+          "pontos_novos_skus": 492,
+          "pontos_pedidos_valor": 29,
+          "pontos_total": 521
+        },
+        {
+          "rca": 450,
+          "vendedor": "Leandro Souza",
+          "pedidos": 18,
+          "positivacoes": 3,
+          "novos_skus": 83,
+          "faturamento": 48959.64,
+          "pontos_positivacao": 15,
+          "pontos_novos_skus": 332,
+          "pontos_pedidos_valor": 25,
+          "pontos_total": 372
+        },
+        {
+          "rca": 144,
+          "vendedor": "Diogo Raposo",
+          "pedidos": 27,
+          "positivacoes": 1,
+          "novos_skus": 37,
+          "faturamento": 63046.28,
+          "pontos_positivacao": 5,
+          "pontos_novos_skus": 148,
+          "pontos_pedidos_valor": 36,
+          "pontos_total": 189
+        },
+        {
+          "rca": 471,
+          "vendedor": "Ana Clara Fassano",
+          "pedidos": 12,
+          "positivacoes": 2,
+          "novos_skus": 35,
+          "faturamento": 8965.5,
+          "pontos_positivacao": 10,
+          "pontos_novos_skus": 140,
+          "pontos_pedidos_valor": 10,
+          "pontos_total": 160
+        },
+        {
+          "rca": 153,
+          "vendedor": "Angelo Neves Suzart",
+          "pedidos": 13,
+          "positivacoes": 3,
+          "novos_skus": 30,
+          "faturamento": 25448.95,
+          "pontos_positivacao": 15,
+          "pontos_novos_skus": 120,
+          "pontos_pedidos_valor": 19,
+          "pontos_total": 154
+        },
+        {
+          "rca": 439,
+          "vendedor": "Mateus Cardoso",
+          "pedidos": 10,
+          "positivacoes": 4,
+          "novos_skus": 30,
+          "faturamento": 3992.47,
+          "pontos_positivacao": 20,
+          "pontos_novos_skus": 120,
+          "pontos_pedidos_valor": 2,
+          "pontos_total": 142
+        },
+        {
+          "rca": 419,
+          "vendedor": "Natali de Oliveira",
+          "pedidos": 0,
+          "positivacoes": 0,
+          "novos_skus": 0,
+          "faturamento": 0.0,
+          "pontos_positivacao": 0,
+          "pontos_novos_skus": 0,
+          "pontos_pedidos_valor": 0,
+          "pontos_total": 0
+        }
+      ]
+    },
+    {
+      "id": "CONVENIENCE",
+      "nome": "Convenience",
+      "premio": 800,
+      "criterios": [
+        {
+          "label": "cliente positivado (Linha Gourmet)",
+          "valor": "+1 pt"
+        }
+      ],
+      "vendedores": [
+        {
+          "rca": 156,
+          "vendedor": "Marilena Tragel",
+          "clientes_gourmet": 0,
+          "faturamento": 26877.26,
+          "faturamento_gourmet": 0.0,
+          "pontos_total": 0
+        },
+        {
+          "rca": 379,
+          "vendedor": "Jorge Maciel",
+          "clientes_gourmet": 0,
+          "faturamento": 26179.8,
+          "faturamento_gourmet": 0.0,
+          "pontos_total": 0
+        },
+        {
+          "rca": 378,
+          "vendedor": "Fabio Valotti",
+          "clientes_gourmet": 0,
+          "faturamento": 10541.47,
+          "faturamento_gourmet": 0.0,
+          "pontos_total": 0
+        },
+        {
+          "rca": 431,
+          "vendedor": "Adeilson Gonçalvez",
+          "clientes_gourmet": 0,
+          "faturamento": 0.0,
+          "faturamento_gourmet": 0.0,
+          "pontos_total": 0
+        }
+      ]
     }
   ],
-  "fontes_indisponiveis": [
-    "CASTAS"
-  ]
+  "fontes_indisponiveis": []
 };
