d4fb8747da58aca11f45e678f8a0dd853c85333c
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-07-10 14:31:15 -0300

    Atualiza nao_pos_sp_data.js - 10/07/2026

--- nao_pos_sp_data.js
+++ nao_pos_sp_data.js
@@ -1,7 +1,7 @@
 // Gerado automaticamente pelo exportacao_nao_pos_sp.py
 
 const NAO_POS_SP_DATA = {
-  "atualizado_em": "10/07/2026 14:26",
+  "atualizado_em": "10/07/2026 14:31",
   "por_vendedor": {
     "ADER ALONSO": [
       {
