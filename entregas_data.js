12a6150ba8d734c32918ff24b00429c8b5fed3a0
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-08-06 09:20:59 -0300

    Alerta Logística RJ: 9 NF(s) não entregues - 2026-08-06

--- entregas_data.js
+++ entregas_data.js
@@ -2262,7 +2262,24 @@ const ENTREGAS_DATA = {
           ]
         }
       ],
-      "nao_entregue": []
+      "nao_entregue": [
+        {
+          "numped": "",
+          "numnota": "418219",
+          "data": "",
+          "cliente": "SUPERMERCADOS RIO SUL DA PENHA LTDA",
+          "placa": "",
+          "rota": "LTG4A18 / PENHA + ZÉ",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 0.0,
+          "itens": [],
+          "motivo_alerta": "LOCAL FECHADO - LOGÍSTICA IGNOROU O AGENDAMENTO — Horário de recebimento só até às 15h, vendedor Angelo Neves está ciente.",
+          "responsavel_alerta": "Logística"
+        }
+      ]
     },
     {
       "nome": "BARBARA CABRAL - OFF TRADE",
@@ -5095,7 +5112,56 @@ const ENTREGAS_DATA = {
           ]
         }
       ],
-      "nao_entregue": []
+      "nao_entregue": [
+        {
+          "numped": "",
+          "numnota": "7251",
+          "data": "",
+          "cliente": "TMT COMERCIO E DISTRIBUICAO DE ALIMENTOS E BEBIDAS LTDA",
+          "placa": "",
+          "rota": "TUK2F64 / TRANSBORDO + TMT COMERCIO",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 0.0,
+          "itens": [],
+          "motivo_alerta": "CANCELAMENTO — Cliente não quis aceitar a mercadoria devido estar em uma caixa diferente , mas a caixa só foi para servir de proteção para o transbordo ser mais seguro. Vendedora Giselle Barfer está ciente e solicitou o cancelamento da NF.",
+          "responsavel_alerta": "Vendas"
+        },
+        {
+          "numped": "",
+          "numnota": "418573",
+          "data": "",
+          "cliente": "ADRIANA BARBOSA DA SILVA CAFETERIA LTDA",
+          "placa": "",
+          "rota": "RKR6E39 / BÚZIOS",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 0.0,
+          "itens": [],
+          "motivo_alerta": "Pendente de Baixa",
+          "responsavel_alerta": "Logística"
+        },
+        {
+          "numped": "",
+          "numnota": "415970",
+          "data": "",
+          "cliente": "32.178.541 CARLOS ALBERTO DE SA MENDONCA JUNIOR",
+          "placa": "",
+          "rota": "RKR6E39 / BÚZIOS",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 0.0,
+          "itens": [],
+          "motivo_alerta": "Pendente de Baixa",
+          "responsavel_alerta": "Logística"
+        }
+      ]
     },
     {
       "nome": "IVANILDO MAIA - OFF TRADE",
@@ -6168,7 +6234,24 @@ const ENTREGAS_DATA = {
           ]
         }
       ],
-      "nao_entregue": []
+      "nao_entregue": [
+        {
+          "numped": "",
+          "numnota": "7258",
+          "data": "",
+          "cliente": "RAMAR COM PRODUTOS ALIMENTICIOS EIRELI",
+          "placa": "",
+          "rota": "LMV5I35 / SANTA CRUZ+ CAMPO GRANDE",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 0.0,
+          "itens": [],
+          "motivo_alerta": "ESTABELECIMENTO COM NECESSIDADES ESPECIAIS — MERCADO SÓ RECEBE A NOTA ATE 14:30H VENDEDOR JORGE MACIEL CIENTE",
+          "responsavel_alerta": "Vendas"
+        }
+      ]
     },
     {
       "nome": "JOSE MARCELO CARDOSO - OFF TRADE",
@@ -7504,7 +7587,40 @@ const ENTREGAS_DATA = {
           ]
         }
       ],
-      "nao_entregue": []
+      "nao_entregue": [
+        {
+          "numped": "",
+          "numnota": "6964",
+          "data": "",
+          "cliente": "DOM ATACAREJO S.A.",
+          "placa": "",
+          "rota": "LSQ9E43 / DOM ATACAREJO",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 0.0,
+          "itens": [],
+          "motivo_alerta": "Nf não estava agendada para entrega, vendedora Maria luiza está ciente — Nf não estava agendada para entrega, vendedora Maria luiza está ciente",
+          "responsavel_alerta": "A Definir"
+        },
+        {
+          "numped": "",
+          "numnota": "7014",
+          "data": "",
+          "cliente": "DOM ATACAREJO S.A.",
+          "placa": "",
+          "rota": "LSQ9E43 / DOM ATACAREJO",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 0.0,
+          "itens": [],
+          "motivo_alerta": "Nf não estava agendada para entrega, vendedora Maria luiza está ciente — Nf não estava agendada para entrega, vendedora Maria luiza está ciente",
+          "responsavel_alerta": "A Definir"
+        }
+      ]
     },
     {
       "nome": "MARILENA TRAGEL - OFF TRADE",
@@ -7801,26 +7917,6 @@ const ENTREGAS_DATA = {
               "desc": "AGUA MINERAL MINALBA S/ GAS 12X510ML",
               "qt": 10,
               "val": 209.0
-            }
-          ]
-        },
-        {
-          "numped": "156003791",
-          "numnota": "418739.0",
-          "data": "04/08/2026",
-          "cliente": "POSTO PALACIO GUANABARA LTDA",
-          "placa": "TTI6D70",
-          "rota": "SULL 2",
-          "status_ped": "",
-          "status_log": "",
-          "motivo": "",
-          "obs": "",
-          "total": 55.5,
-          "itens": [
-            {
-              "desc": "NOSSO CHOP RED DRAFT VD 6X600ML",
-              "qt": 1,
-              "val": 55.5
             }
           ]
         },
@@ -9235,7 +9331,30 @@ const ENTREGAS_DATA = {
           ]
         }
       ],
-      "nao_entregue": []
+      "nao_entregue": [
+        {
+          "numped": "156003791",
+          "numnota": "418739.0",
+          "data": "04/08/2026",
+          "cliente": "POSTO PALACIO GUANABARA LTDA",
+          "placa": "TTI6D70",
+          "rota": "SULL 2",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 55.5,
+          "itens": [
+            {
+              "desc": "NOSSO CHOP RED DRAFT VD 6X600ML",
+              "qt": 1,
+              "val": 55.5
+            }
+          ],
+          "motivo_alerta": "ERRO DE ROTA — Fora de rota.",
+          "responsavel_alerta": "Logística"
+        }
+      ]
     },
     {
       "nome": "MATEUS CARDOSO - OFF TRADE",
@@ -11661,7 +11780,24 @@ const ENTREGAS_DATA = {
       "nao_emitido": [],
       "emitido_s_rota": [],
       "canceladas": [],
-      "nao_entregue": []
+      "nao_entregue": [
+        {
+          "numped": "",
+          "numnota": "418318",
+          "data": "",
+          "cliente": "38.200.029 CLAUDIO LUIZ PEREIRA FARIA",
+          "placa": "",
+          "rota": "RKR6E39 / BÚZIOS",
+          "status_ped": "",
+          "status_log": "",
+          "motivo": "",
+          "obs": "",
+          "total": 0.0,
+          "itens": [],
+          "motivo_alerta": "Pendente de Baixa",
+          "responsavel_alerta": "Logística"
+        }
+      ]
     }
   ]
 };
