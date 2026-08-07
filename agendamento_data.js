4171020ef013e946af7aa98d249fe7cbc1ebd020
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-08-07 13:31:07 -0300

    Atualiza agendamento_data.js (planilha) - 07/08/2026

--- agendamento_data.js
+++ agendamento_data.js
@@ -25728,7 +25728,7 @@ const AGENDAMENTO_DATA = {
       ]
     }
   ],
-  "atualizado_em": "07/08/2026 13:10",
+  "atualizado_em": "07/08/2026 13:31",
   "comparativo": [
     {
       "msg_id": "19fd70e8e7c7167a",
