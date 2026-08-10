15ac746ee9de942778d30694221a30b413f6c731
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-08-10 17:17:23 -0300

    Atualiza clientes_inativos_data.js - 10/08/2026

--- clientes_inativos_data.js
+++ clientes_inativos_data.js
@@ -1,7 +1,7 @@
 // Gerado automaticamente
 
 const INATIVOS_DATA = {
-  "atualizado_em": "10/08/2026 17:00",
+  "atualizado_em": "10/08/2026 17:17",
   "por_vendedor": {
     "ADEILSON GONÇALVEZ - OFF TRADE": {
       "rca": "431",
