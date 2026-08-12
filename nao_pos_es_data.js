727eaf0926f27f501a934155967157e47761657a
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-08-12 15:42:29 -0300

    Atualiza nao_pos_es_data.js - 12/08/2026

--- nao_pos_es_data.js
+++ nao_pos_es_data.js
@@ -1,7 +1,7 @@
 // Gerado automaticamente pelo exportacao_nao_pos_es.py
 
 const NAO_POS_ES_DATA = {
-  "atualizado_em": "12/08/2026 15:15",
+  "atualizado_em": "12/08/2026 15:42",
   "por_vendedor": {
     "ADEILSON GONÇALVEZ": [
       {
