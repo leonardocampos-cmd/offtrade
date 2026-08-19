4592184ceea552b494d9299d916e6a77502c9295
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-08-19 09:17:09 -0300

    Atualiza acao_amarula_data.js - 19/08/2026

---
+++ acao_amarula_data.js
@@ -0,0 +1,25 @@
+const ACAO_AMARULA_DATA = {
+  "atualizado_em": "19/08/2026 09:17",
+  "periodo": {
+    "ini": "19/08/2026",
+    "fim": "19/09/2026"
+  },
+  "escopo": "Time RJ · Vendedores Externo",
+  "item": {
+    "codigo": 7702,
+    "descricao": "Amarula Cream 750ml + 1 Copo",
+    "valor": 98.9
+  },
+  "meta_minima": 50,
+  "premios": [
+    800,
+    600,
+    300,
+    200
+  ],
+  "total_vendedores": 0,
+  "total_volume": 0,
+  "total_faturamento": 0.0,
+  "ranking": [],
+  "fontes_indisponiveis": []
+};
