af57b63f88c79f535c9b1043e1ba9e5766aa55d2
Leonardo Campos <leonardo.campos@rigarr.com.br>
2026-08-19 10:05:31 -0300

    Atualiza acao_amarula_data.js - 19/08/2026

--- acao_amarula_data.js
+++ acao_amarula_data.js
@@ -1,5 +1,5 @@
 const ACAO_AMARULA_DATA = {
-  "atualizado_em": "19/08/2026 09:17",
+  "atualizado_em": "19/08/2026 10:05",
   "periodo": {
     "ini": "19/08/2026",
     "fim": "19/09/2026"
@@ -17,9 +17,199 @@ const ACAO_AMARULA_DATA = {
     300,
     200
   ],
-  "total_vendedores": 0,
+  "total_vendedores": 27,
   "total_volume": 0,
   "total_faturamento": 0.0,
-  "ranking": [],
+  "ranking": [
+    {
+      "vendedor": "ADEILSON GONÇALVEZ",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "ALEXSANDRO DOS SANTOS",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "ALLAN PAES",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "ANA CLARA FASSANO",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "ANDRESSA BARROS FERREIRA",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "ANGELO NEVES SUZART",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "ARLLEY FIGUEIREDO",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "BARBARA CABRAL",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "BRUNO FRANCISCO",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "CLAUDIO NOBREGA",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "DANIELLE MOURA",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "DIOGO RAPOSO",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "DIRLEI ARAUJO",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "FABIO VALOTTI",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "FERNANDA DE OLIVEIRA",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "FRANCISCO CARLOS MACIEL",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "FÁBIO OLIVEIRA",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "JORGE MACIEL",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "JOSE MARCELO CARDOSO",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "LEANDRO SOUZA",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "LORRAYNE SANTOS",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "MARIA LUIZA",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "MARILENA TRAGEL",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "MATEUS CARDOSO",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "MAYCON COELHO FERRAZ",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "NÁTALI DE OLIVEIRA",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    },
+    {
+      "vendedor": "VIVIANI ALVES",
+      "volume": 0,
+      "faturamento": 0.0,
+      "elegivel": false,
+      "premio": null
+    }
+  ],
   "fontes_indisponiveis": []
 };
